043a288dc545d9eef2a4214644be819c220197f5
KaterinaLarionova <lovadza@mail.ru>
2024-11-08 21:01:46 +0300

    add photos

---
+++ js/gallery.js
@@ -0,0 +1,16 @@
+import {photos} from './data.js';
+
+const pictureList = document.querySelector('.pictures');
+const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
+const listFragment = document.createDocumentFragment();
+
+photos.forEach(({url, description, likes, comments}) =>{
+  const pictureElement = templatePicture.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments;
+  listFragment.appendChild(pictureElement);
+});
+
+pictureList.appendChild(listFragment);
