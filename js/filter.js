f7f3dc659641ec502d2a52c695bd19db0e06e21a
KaterinaLarionova <lovadza@mail.ru>
2024-12-05 13:32:12 +0300

    фильтрация фотографий

---
+++ js/filter.js
@@ -0,0 +1,52 @@
+import {createPictures} from './gallery.js';
+import {debounce} from './util.js';
+
+const RENDER_DELAY = 500;
+
+const PICTURE_COUNT = 10;
+const FILTER = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed',
+};
+const filtersElement = document.querySelector('.img-filters');
+let currentFilter = '';
+let pictures = [];
+
+const turnFilterOn = (loadPictures) => {
+  filtersElement.classList.remove('img-filters--inactive');
+  pictures = [...loadPictures];
+  currentFilter = FILTER.DEFAULT;
+};
+
+const filterPictures = () => {
+  if (currentFilter === FILTER.RANDOM) {
+    return [...pictures].sort(() => Math.random() - 0.5).slice(0, PICTURE_COUNT);
+  } else if (currentFilter === FILTER.DISCUSSED) {
+    return [...pictures].sort((a, b) => b.comments.length - a.comments.length);
+  } else {
+    return [...pictures];
+  }
+};
+
+const debounceCreatePictures = debounce(createPictures, RENDER_DELAY);
+
+filtersElement.addEventListener('click', (evt) => {
+  if (!evt.target.classList.contains('img-filters__button')) {
+    return;
+  }
+  const clickedButton = evt.target;
+  if (clickedButton.id === currentFilter) {
+    return;
+  }
+
+  filtersElement.querySelector('.img-filters__button--active')
+    .classList.remove('img-filters__button--active');
+
+  clickedButton.classList.add('img-filters__button--active');
+  currentFilter = clickedButton.id;
+  debounceCreatePictures(filterPictures());
+});
+
+export {turnFilterOn, filterPictures};
+
