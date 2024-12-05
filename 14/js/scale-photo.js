198e91f22e0a691744f1986b074aed69538c5dc6
Keks <keks@htmlacademy.ru>
2024-12-05 16:51:27 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/scale-photo.js
@@ -0,0 +1,46 @@
+const FILE_TYPES = ['jpg', 'jpeg', 'png', 'gif'];
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+const SCALE_STEP = 25;
+const editImage = document.querySelector('.img-upload__preview img');
+const scaleInput = document.querySelector('.scale__control--value');
+const minesButton = document.querySelector('.scale__control--smaller');
+const plusButton = document.querySelector('.scale__control--bigger');
+const fileChooser = document.querySelector('.img-upload__input');
+
+fileChooser.addEventListener('change', () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((it) => {
+    return fileName.endsWith(it);
+  });
+
+  if (matches) {
+    editImage.src = URL.createObjectURL(file);
+  }
+});
+const scaleImage = (value = MAX_SCALE) => {
+  editImage.style.transform = `scale(${value / MAX_SCALE})`;
+  scaleInput.value = `${value}%`;
+};
+
+const onScaleButtonClick = (isIncrease) => {
+  const scaleValue = parseInt(scaleInput.value, 10);
+  let newScaleNumber = isIncrease ? scaleValue + SCALE_STEP : scaleValue - SCALE_STEP;
+  if (newScaleNumber < MIN_SCALE) {
+    newScaleNumber = MIN_SCALE;
+  } else if (newScaleNumber > MAX_SCALE) {
+    newScaleNumber = MAX_SCALE;
+  }
+
+  scaleImage(newScaleNumber);
+};
+
+minesButton.addEventListener('click', () => onScaleButtonClick(false));
+plusButton.addEventListener('click', () => onScaleButtonClick(true));
+
+const resetScale = () => {
+  scaleImage(MAX_SCALE);
+};
+
+export {resetScale};
